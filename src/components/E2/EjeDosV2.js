cf3289a717fb8d9328923219746d43596bf44300
Vazquez Cesar <cesar.evc@outlook.com>
2020-07-02 13:04:42 -0500

    ejercicio 2

--- src/components/E2/EjeDosV2.js
+++ src/components/E2/EjeDosV2.js
@@ -1,66 +1,16 @@
 import React,  { useState } from 'react';
-import style from './EjeDos.module.css';
+
+import styles from './EjeDos.module.css';
 
 function EjeDosV2 () {
     const [count, setCount] = useState(0);
 
     return (
         <div>
-            <button 
-                onClick={() => setCount(count + 1)} 
-                className={style.btn_clss} 
-                style={{
-                    margin: '4',
-                    background: "black",
-                    color: "white",
-                    padding: 8,
-                    borderRadius: 8,
-                    textTransform: "uppercase",
-                    paddingLeft: 32,
-                    paddingRight: 32
-                }} 
-            >Add</button>
-            <button 
-                onClick={() => setCount(count - 2)} 
-                className={style.btn}
-                style={{
-                    margin: '4',
-                    background: "black",
-                    color: "white",
-                    padding: 8,
-                    borderRadius: 8,
-                    textTransform: "uppercase",
-                    paddingLeft: 32,
-                    paddingRight: 32
-                }} 
-            >Substract</button>
-            <button 
-                onClick={() => setCount(count / 3)} 
-                className={style.btn} 
-                style={{
-                    margin: '4',
-                    background: "black",
-                    color: "white",
-                    padding: 8,
-                    borderRadius: 8,
-                    textTransform: "uppercase",
-                    paddingLeft: 32,
-                    paddingRight: 32
-                }} 
-            >Divide</button>
-            <button 
-                onClick={() => setCount(0)} 
-                style={{
-                    margin: '4',
-                    background: "black",
-                    color: "white",
-                    padding: 8,
-                    borderRadius: 8,
-                    textTransform: "uppercase",
-                    paddingLeft: 32,
-                    paddingRight: 32
-                }}  
-            >Reset</button>
+            <button onClick={() => setCount(count + 1)} className={styles.buttonModified}>Add</button>
+            <button onClick={() => setCount(count - 2)} className={styles.buttonModified}>Substract</button>
+            <button onClick={() => setCount(count / 3)} className={styles.buttonModified}>Divide</button>
+            <button onClick={() => setCount(0)} className={styles.buttonModified}>Reset</button>
 
 
             <div style={{background: count%3 === 0 ? 'red' : count%3 === 1 ? 'green' : count%3 === 2 ? 'blue' : 'white'} }>
